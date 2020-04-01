bf5095e6e3e2b9720c46e494b7fd8ba00949b785
Phillip Paul <phillippaul@gmx.de>
2020-04-01 21:13:58 +0200

    starting to work in index.js, issue with toggle

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,45 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+  shuffleCards(array) {
+    // from 'bost.ocks.org/mike/shuffle/':
+    let m = array.length,
+      t,
+      i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+
+    if (card1 == card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isFinished() {
+    if (this.cards.length / 2 == this.pairsGuessed) {
+      return "Game finished";
+    } else {
+      return "A little more";
+    }
+  }
 }
